8f2ecf891920308d321d278062897ec97988bb0e
Сергей Старченко < @DESKTOP-Q8JJKNE>
2024-03-11 03:02:02 +0300

    Подготовка проекта

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine( "wdeowe,dwo,rdmwormd" );
         }
     }
 }
